bb55b6e748b8168f463bb6f5d4bb79cd0e09557a
durudaniel <danielchiemela16@gmail.com>
2025-09-20 18:11:04 +0100

    Defining a React-Router package

--- worldWise/src/App.jsx
+++ worldWise/src/App.jsx
@@ -1,3 +1,12 @@
-export default function () {
-  return <div>World wise</div>;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./Pages/Product";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }

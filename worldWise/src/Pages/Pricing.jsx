7f1a61b31623cfb3f0b6bd644165d6fede2e4be0
durudaniel <danielchiemela16@gmail.com>
2025-09-21 20:29:06 +0100

    Creating a Pricing React Component

--- worldWise/src/Pages/Pricing.jsx
+++ worldWise/src/Pages/Pricing.jsx
@@ -1,4 +1,4 @@
-import PageNav from "../Components/PageNav";
+/*import PageNav from "../Components/PageNav";
 
 export default function Pricing() {
   return (
@@ -6,5 +6,30 @@ export default function Pricing() {
       <PageNav />
       <h1>Pricing</h1>
     </div>
+  );
+}*/
+
+// Uses the same styles as Product
+import styles from "./Product.module.css";
+
+export default function Product() {
+  return (
+    <main className={styles.product}>
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+      </section>
+    </main>
   );
 }

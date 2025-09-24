df35939063cb41448851715e37ac30f85c1f1766
durudaniel <danielchiemela16@gmail.com>
2025-09-24 16:21:23 +0100

    Commiting node-modules and a json package

--- worldWise/src/App.jsx
+++ worldWise/src/App.jsx
@@ -5,18 +5,25 @@ import Pricing from "./Pages/Pricing";
 import PageNotFound from "./Pages/PageNotFound";
 import AppLayout from "./Pages/AppLayout";
 import Login from "./Pages/Login";
+import CityList from "./Components/CityList";
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="*" element={<HomePage />}></Route>
           <Route path="/product" element={<Product />}></Route>
           <Route path="/pricing" element={<Pricing />}></Route>
           <Route path="/Login" element={<Login />}></Route>
-          <Route path="/app" element={<AppLayout />}></Route>
+          <Route path="/app" element={<AppLayout />}>
+            <Route index element={<CityList />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="*" element={<PageNotFound />}></Route>
+          <Route />
         </Routes>
       </BrowserRouter>
     </div>
